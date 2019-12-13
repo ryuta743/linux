c82ec97a67334477083a047b938f424d1a115602
Ryuta <revos347@gmail.com>
2019-12-13 15:34:48 +0900

    mariadb v1.3
    うごけ！

--- tenshoku/docker/db/sql/002-init-database.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# shell script announce 
-
-#wait for the MySQL Server to come up
-sleep 90s
--- tenshoku/docker/db/sql/003-create-tables.sql
+++ tenshoku/docker/db/sql/003-create-tables.sql
@@ -1,20 +1,9 @@
-USE mydb;
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+use mydb;
 
-DROP table IF EXISTS userLists;
-
----- create ----
-create table userLists
-(
-    id INT(10) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE users (
+  id int(10) unsigned not null auto_increment,
+  name varchar(255) not null,
+  created_time datetime not null default current_timestamp,
+  updated_time datetime not null default current_timestamp on update current_timestamp,
+  primary key (id)
 );
-
----- record of userLists ----
-BEGIN;
-INSERT INTO `userLists` ('id, password') VALUES ('123, root');
-INSERT INTO `userLists` ('id, password') VALUES ('455, root');
-COMMIT;
-
