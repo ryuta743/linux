7b4e7e5f59c2d1989d95c7c653ad521bab231c9c
Ryuta <revos347@gmail.com>
2019-12-15 17:08:15 +0900

    api v1.2
    asdf

--- tenshoku/docker/api/src/app.js
+++ tenshoku/docker/api/src/app.js
@@ -15,7 +15,9 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,3 +41,26 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// mysqlを読み込む
+var mysql = require('mysql');
+
+// mysqlと接続する
+var db = mysql.createConnection({
+  // host: 'mariadb',
+  host: 'http://db:3306/',
+  user: 'root',
+  password: 'root',
+  database: 'mydb'
+});
+
+// userListsを表示
+app.get('/showUserLists', function (req, res) {
+  const id = req.query.id;
+  const sql = `SELECT * FROM userLists;`;
+  db.query(sql, function (error, results) {
+    if (error) return res.json(error);
+    res.json(results);
+    //console.log(results);
+  })
+})
