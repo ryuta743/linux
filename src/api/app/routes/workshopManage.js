f847bf9ee8ae868948b9a65f937273cc2b28be9d
Ryuta <ryuta7435@gmail.com>
2020-01-07 18:58:40 +0900

    k8s v0.1

---
+++ src/api/app/routes/workshopManage.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+
+// MySQLの設定情報
+var mysql_setting = {
+    host: 'db_mysql',
+    user: 'root',
+    password: '',
+    database: 'tenshoku',
+};
+
+//ショップ情報をショップidから引き出す
+router.get('/getShopdata', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    const shop_id = req.query.shop_id;
+    var connection = mysql.createConnection(mysql_setting);
+    connection.connect();
+    connection.query('SELECT * from workshop_lists where shop_id=?', shop_id,
+        function (error, results, fields) {
+            if (error == null) {
+                if (results.length > 0) {
+                    return res.json(results);
+                } else {
+                    return res.status(403).json('You dont have workshop')
+                }
+            } else {
+                console.log('エラーはいてるよ' + error)
+                return res.status(504)
+            }
+        })
+    connection.end();
+});
+
+router.get('/getOrderlist', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    const shop_id = req.query.shop_id;
+    var connection = mysql.createConnection(mysql_setting);
+    connection.connect();
+    connection.query('SELECT * from order_lists where shop_id=?', shop_id,
+        function (error, results, fields) {
+            if (error == null) {
+                if (results.length > 0) {
+                    return res.json(results);
+                } else {
+                    return res.status(403)
+                }
+            } else {
+                console.log('エラーはいてるよ' + error)
+                return res.status(504)
+            }
+        })
+    connection.end();
+});
+
+router.get('/getOrderdetail', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    const shop_id = req.query.shop_id;
+    const order_num = req.query.order_number
+    var connection = mysql.createConnection(mysql_setting);
+    connection.connect();
+    connection.query('SELECT * from order_details INNER JOIN product_lists ON order_details.product_id = product_lists.product_id where order_details.shop_id=? && order_details.order_number=?', [shop_id,order_num],
+        function (error, results, fields) {
+            if (error == null) {
+                if (results.length > 0) {
+                    return res.json(results);
+                } else {
+                    return res.status(403)
+                }
+            } else {
+                console.log('エラーはいてるよ' + error)
+                return res.status(504)
+            }
+        })
+    connection.end();
+});
+
+router.get('/getProducts', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    const shop_id = req.query.shop_id;
+    var connection = mysql.createConnection(mysql_setting);
+    connection.connect();
+    connection.query('SELECT * from product_lists WHERE shop_id=? ', shop_id,
+        function (error, results, fields) {
+            if (error == null) {
+                if (results.length > 0) {
+                    return res.json(results);
+                } else {
+                    return res.status(403)
+                }
+            } else {
+                console.log('エラーはいてるよ' + error)
+                return res.status(504)
+            }
+        })
+    connection.end();
+});
+
+module.exports = router;
---
+++ src/db/sql/001-create-tables.sql
@@ -0,0 +1,96 @@
+alter database `tenshoku` character set utf8mb4;
+show variables like 'char%';
+---- drop ----
+DROP TABLE IF EXISTS `mydata`;
+
+---- create ----
+create table IF not exists `mydata`
+(
+ `id` INT(20) AUTO_INCREMENT,
+ `name` VARCHAR(40) NOT NULL,
+ `mail` VARCHAR(40) DEFAULT NULL,
+ `age` INT(3) DEFAULT NULL,
+ `created_at`       Datetime DEFAULT NULL,
+ `updated_at`       Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+ ) DEFAULT CHARSET=utf8mb4;
+
+create table `user_lists`
+(
+  `user_id` INT(8) AUTO_INCREMENT NOT NULL,
+  `user_name` VARCHAR(16) NOT NULL,
+  `user_pass` VARCHAR(50) NOT NULL,
+  `user_mail` VARCHAR(60) NOT NULL,
+  `nationality`	INT(3) NOT NULL,
+  `gender` INT(1) NOT NULL,
+  `birth` DATE NOT NULL,
+  `user_type` INT(1) NOT NULL,
+  `create_day` DATE NOT NULL,
+  PRIMARY KEY (`user_id`)
+) DEFAULT CHARSET=utf8mb4;
+
+create table `workshop_lists`
+(
+  `shop_id` INT(8) AUTO_INCREMENT NOT NULL,
+  `shop_name` VARCHAR(30) NOT NULL,
+  `shop_owner` INT(8) NOT NULL,
+  `shop_description` VARCHAR(280),
+  `create_day`	DATE NOT NULL,
+  `shop_img` VARCHAR(200),
+  `postal_code` INT(7) NOT NULL,
+  `address` VARCHAR(50) NOT NULL,
+  `work_mail` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`shop_id`,`shop_owner`)
+) DEFAULT CHARSET=utf8mb4;
+
+create table `product_lists`
+(
+  `product_id` INT(8) AUTO_INCREMENT NOT NULL,
+  `shop_id` INT(8) NOT NULL,
+  `product_name` VARCHAR(20) NOT NULL,
+  `product_number` INT(4),
+  `price`	INT(10) NOT NULL,
+  `record_date` DATE NOT NULL,
+  `product_detail` VARCHAR(150),
+  `product_img` VARCHAR(200),
+  `stock` INT(10) NOT NULL,
+  `safety` INT(10) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) DEFAULT CHARSET=utf8mb4;
+
+create table `order_lists`
+(
+  `order_number` INT(8) NOT NULL,
+  `user_id` INT(8) NOT NULL,
+  `user_name` VARCHAR(16) NOT NULL,
+  `shop_id` INT(8) NOT NULL,
+  `shop_name` VARCHAR(30) NOT NULL,
+  `buy_date`	DATE NOT NULL,
+  `countory` VARCHAR(30) NOT NULL,
+  `post_address` VARCHAR(50) NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
+  `price_all` INT(12),
+  `status` INT(1) NOT NULL,
+  PRIMARY KEY (`order_number`,`shop_id`)
+) DEFAULT CHARSET=utf8mb4;
+
+create table `order_details`
+(
+  `order_number` INT(8) NOT NULL,
+  `shop_id` INT(8) NOT NULL,
+  `user_id` INT(8) NOT NULL,
+  `user_name` VARCHAR(16) NOT NULL,
+  `product_id` INT(8) NOT NULL,
+  `product_name` VARCHAR(20) NOT NULL,
+  `price` INT(10) NOT NULL,
+  `count`	INT(4) NOT NULL,
+  `proccess` INT(1) NOT NULL,
+  PRIMARY KEY (`order_number`,`shop_id`,`product_id`)
+) DEFAULT CHARSET=utf8mb4;
+
+create table `line_link`
+(
+  `user_id` INT(8) NOT NULL,
+  `line_id` VARCHAR(60) NOT NULL
+) DEFAULT CHARSET=utf8mb4;
+
