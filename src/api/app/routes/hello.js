7af11644b7b12f68d48bbb0f8657edc7ca257ed1
Ryuta <revos743@gmail.com>
2020-02-04 00:52:36 +0900

    a

--- src/api/app/routes/hello.js
+++ src/api/app/routes/hello.js
@@ -7,7 +7,7 @@ var mysql = require('mysql'); //★追加
 
 // MySQLの設定情報
 var mysql_setting = {
-    host: 'db',
+    host: 'db.default.svc.cluster.local',
     user: 'root',
     password: '',
     database: 'tenshoku',
